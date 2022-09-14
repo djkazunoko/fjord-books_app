3303e10df0850a429af39212c13276145f9344d7
djkazunoko <djkazunoko@gmail.com>
2022-09-14 10:52:23 +0900

    letter_opener_webをセットアップ

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :books
   resources :users, only: [:index, :show]
+  
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
