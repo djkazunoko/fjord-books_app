a5333778fca218cec52219c14befb361adda0c2c
djkazunoko <djkazunoko@gmail.com>
2022-09-24 08:13:57 +0900

    フォローしているユーザー・フォロワー

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,18 @@ class User < ApplicationRecord
   has_many :passive_friendships, class_name: "Friendship",
                                  foreign_key: "followed_id",
                                  dependent: :destroy
+  has_many :followings, through: :active_friendships, source: :followed
+  has_many :followers, through: :passive_friendships, source: :follower
+
+  def follow(other_user)
+    followings << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
+  end
 end
