65872ae199f2f042eaa9183660046c5a78d1bf4e
djkazunoko <djkazunoko@gmail.com>
2022-09-23 13:00:16 +0900

    フォロワー

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,11 @@ class User < ApplicationRecord
   has_many :active_friendships, class_name:  "Friendship",
                                 foreign_key: "follower_id",
                                 dependent:   :destroy
+  has_many :passive_friendships, class_name:  "Friendship",
+                                foreign_key: "followed_id",
+                                dependent:   :destroy
   has_many :following, through: :active_friendships, source: :followed
+  has_many :followers, through: :passive_friendships, source: :follower # こっちの:sourceキーは省略可能
 
   # ユーザーをフォローする
   def follow(other_user)
