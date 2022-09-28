3405bddc061e05e5a39d11efd7b794f385304c60
djkazunoko <djkazunoko@gmail.com>
2022-09-28 11:11:20 +0900

    13.3.4 マイクロポストを削除する

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,6 @@
 class MicropostsController < ApplicationController
+  before_action :correct_user, only: :destroy
+
   def index
     @microposts = Micropost.all
   end
@@ -14,11 +16,18 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def micropost_params
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
