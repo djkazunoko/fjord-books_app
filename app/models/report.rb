8de8019666be7e09951fe03a49b90323b7e20e95
djkazunoko <djkazunoko@gmail.com>
2022-09-29 09:27:36 +0900

    CommentとBook・Reportをポリモーフィック関連付け

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,7 @@
 class Report < ApplicationRecord
   belongs_to :user
+  has_many :comments, as: :commentable
+
   validates :user_id, presence: true
   validates :title, presence: true
   validates :content, presence: true
