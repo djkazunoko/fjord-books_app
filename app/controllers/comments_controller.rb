997363b9100758554a60575fd3e70ddfc827a058
djkazunoko <djkazunoko@gmail.com>
2022-09-30 13:21:17 +0900

    コメントを投稿したユーザーの情報を表示する

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @comment = @commentable.comments.build(comment_params)
+    @comment.user = current_user
     if @comment.save
       redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
