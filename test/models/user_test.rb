d5d3d718704ca60210cb486c3714a730e84e22a2
djkazunoko <djkazunoko@gmail.com>
2022-10-10 08:52:09 +0900

    Userクラスのテストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,39 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user1 = users(:one)
+    @user2 = users(:two)
+  end 
+
+  test '#following?' do
+    assert_not @user1.following?(@user2)
+    @user1.follow(@user2)
+    assert @user1.following?(@user2)
+  end
+
+  test '#followed_by?' do
+    assert_not @user1.followed_by?(@user2)
+    @user2.follow(@user1)
+    assert @user1.followed_by?(@user2)
+  end
+
+  test '#follow' do
+    assert_not @user1.following?(@user2)
+    @user1.follow(@user2)
+    assert @user1.following?(@user2)
+  end
+
+  test '#unfollow' do
+    @user1.follow(@user2)
+    assert @user1.following?(@user2)
+    @user1.unfollow(@user2)
+    assert_not @user1.following?(@user2)
+  end
+
+  test "#name_or_email" do
+    assert_equal '1@example.com', @user1.name_or_email
+    @user1.name = 'one'
+    assert_equal 'one', @user1.name_or_email
+  end
 end
