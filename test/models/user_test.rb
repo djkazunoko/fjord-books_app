5910df8886565e71f0bf34827a26d16bf4707bd6
djkazunoko <djkazunoko@gmail.com>
2022-10-17 08:35:48 +0900

    Userクラスのテストを修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,29 +8,15 @@ class UserTest < ActiveSupport::TestCase
     @user2 = users(:two)
   end
 
-  test '#following?' do
+  test '#following?, #followed_by?, #follow and #unfollow' do
     assert_not @user1.following?(@user2)
+    assert_not @user2.followed_by?(@user1)
     @user1.follow(@user2)
     assert @user1.following?(@user2)
-  end
-
-  test '#followed_by?' do
-    assert_not @user1.followed_by?(@user2)
-    @user2.follow(@user1)
-    assert @user1.followed_by?(@user2)
-  end
-
-  test '#follow' do
-    assert_not @user1.following?(@user2)
-    @user1.follow(@user2)
-    assert @user1.following?(@user2)
-  end
-
-  test '#unfollow' do
-    @user1.follow(@user2)
-    assert @user1.following?(@user2)
+    assert @user2.followed_by?(@user1)
     @user1.unfollow(@user2)
     assert_not @user1.following?(@user2)
+    assert_not @user2.followed_by?(@user1)
   end
 
   test '#name_or_email' do
